c4985c17197d6b6297f54e10fe839b50dd6a77f9
Scott Kennedy <skennedy27@gmail.com>
2020-06-23 19:55:16 -0700

    Add GitHub link

--- src/main/kotlin/com/canadiancow/aqd/AqdCalculator.kt
+++ src/main/kotlin/com/canadiancow/aqd/AqdCalculator.kt
@@ -100,7 +100,8 @@ class AqdCalculator(
     private fun getHeader(): String {
         val builder = StringBuilder()
         builder.append("<html>\n<head><title>AQD Calculator</title></head>\n<body>\n")
-        builder.append("""<div><a href="https://www.flyertalk.com/forum/air-canada-aeroplan/1744575-new-improved-calculator-aqm-aeroplan-miles-aqd.html" target="_blank">FlyerTalk Thread</a></div><br/>""")
+        builder.append("""<div><a href="https://www.flyertalk.com/forum/air-canada-aeroplan/1744575-new-improved-calculator-aqm-aeroplan-miles-aqd.html">FlyerTalk Thread</a><br/>""")
+        builder.append("""<a href="https://github.com/scottkennedy/ac-aqd">GitHub Repository</a></div><br/>""")
         return builder.toString()
     }
 
