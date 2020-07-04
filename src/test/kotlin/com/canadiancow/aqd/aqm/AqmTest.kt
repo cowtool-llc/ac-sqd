f57c110d17d9b3a1f19e16ae9c857e58676abdf1
Scott Kennedy <scottkennedy@users.noreply.github.com>
2020-07-03 18:16:15 -0700

    Update AqmTest.kt

--- src/test/kotlin/com/canadiancow/aqd/aqm/AqmTest.kt
+++ src/test/kotlin/com/canadiancow/aqd/aqm/AqmTest.kt
@@ -20,7 +20,7 @@ internal class AqmTest {
             )
         )
         assertEquals(
-            125,
+            1245,
             getEarningResult(
                 "AC",
                 "SFO",
