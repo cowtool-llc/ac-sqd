c97190685ecf6d48dba84161768de33786036b77
Scott Kennedy <skennedy27@gmail.com>
2022-10-18 22:55:06 -0700

    Add link to new site

--- src/main/kotlin/com/canadiancow/sqd/SqdCalculator.kt
+++ src/main/kotlin/com/canadiancow/sqd/SqdCalculator.kt
@@ -126,6 +126,7 @@ class SqdCalculator(
     private fun getHeader(): String {
         val builder = StringBuilder()
         builder.append("<html>\n<head><title>SQD Calculator</title></head>\n<body>\n")
+        builder.append("""<div style="color: red;">This calculator is moving to a new location: Please update your bookmarks. <a href="https://acsqd.cowtool.com/">https://acsqd.cowtool.com/</a></div><br/>""")
         builder.append("""<div><a href="https://www.flyertalk.com/forum/air-canada-aeroplan/1744575-new-improved-calculator-aqm-aeroplan-miles-aqd.html">FlyerTalk Thread</a><br/>""")
         builder.append("""<a href="https://github.com/scottkennedy/ac-aqd">GitHub Repository</a></div><br/>""")
         return builder.toString()
