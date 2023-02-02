c8158c03e1ce81357b661a605f3888d71b83424d
Scott Kennedy <scottkennedy@users.noreply.github.com>
2023-02-01 16:32:18 -0800

    Update Calculators.kt

--- src/main/kotlin/com/cowtool/acsqd/sqm/Calculators.kt
+++ src/main/kotlin/com/cowtool/acsqd/sqm/Calculators.kt
@@ -294,11 +294,7 @@ private val acCalculator: EarningCalculator =
             }
 
             val trueBasis = split[0]
-            val brand = if (trueBasis.length >= 3) {
-                trueBasis.substring(trueBasis.length - 2, trueBasis.length)
-            } else {
-                null
-            }
+            val brand = trueBasis.takeLast(2)
 
             when (brand) {
                 "BA", "GT" ->
