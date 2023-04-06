2e6ebd356cf65b0bf032fbc130df6f7e4842ca53
Scott Kennedy <skennedy27@gmail.com>
2023-04-06 08:47:39 -0400

    more tests, and a fix

--- src/test/kotlin/com/cowtool/acsqd/SqdCalculatorTest.kt
+++ src/test/kotlin/com/cowtool/acsqd/SqdCalculatorTest.kt
@@ -26,19 +26,84 @@ internal class SqdCalculatorTest {
      */
     @Test
     fun `SQD rounding should match AC`() {
-        val result = SqdCalculator(
-            baseFare = 829.0,
-            surcharges = 0.0,
-            ticket = "014",
-            aeroplanStatus = "100",
-            hasBonusPointsPrivilege = true,
-            segments = """
+        with(
+            SqdCalculator(
+                baseFare = 829.0,
+                surcharges = 0.0,
+                ticket = "014",
+                aeroplanStatus = "100",
+                hasBonusPointsPrivilege = true,
+                segments = """
                 AC,SFO,YVR,Y,LT
                 AC,YVR,YYZ,Y,LT
             """.trimIndent(),
-        ).calculate()
+            ).calculate()
+        ) {
+            assertEquals(231, itinerary!!.segments[0].earningResult!!.sqd)
+            assertEquals(599, itinerary!!.segments[1].earningResult!!.sqd)
+        }
 
-        assertEquals(231, result.itinerary!!.segments[0].earningResult!!.sqd)
-        assertEquals(599, result.itinerary!!.segments[1].earningResult!!.sqd)
+        with(
+            SqdCalculator(
+                baseFare = 1217.0,
+                surcharges = 0.0,
+                ticket = "014",
+                aeroplanStatus = "100",
+                hasBonusPointsPrivilege = true,
+                segments = """
+                AC,YYC,EWR,Y,LT
+                AC,EWR,YYC,Y,LT
+            """.trimIndent(),
+            ).calculate()
+        ) {
+            assertEquals(609, itinerary!!.segments[0].earningResult!!.sqd)
+            assertEquals(609, itinerary!!.segments[1].earningResult!!.sqd)
+        }
+
+        with(
+            SqdCalculator(
+                baseFare = 3610.0,
+                surcharges = 0.0,
+                ticket = "014",
+                aeroplanStatus = "100",
+                hasBonusPointsPrivilege = true,
+                segments = """
+                AC,YYC,YUL,Y,LT
+                AC,YUL,GRU,Y,LT
+                AC,GRU,EZE,Y,LT
+                AC,EZE,GRU,Y,LT
+                AC,GRU,YUL,Y,LT
+                AC,YUL,YYC,Y,LT
+            """.trimIndent(),
+            ).calculate()
+        ) {
+            assertEquals(422, itinerary!!.segments[0].earningResult!!.sqd)
+            assertEquals(1142, itinerary!!.segments[1].earningResult!!.sqd)
+            assertEquals(242, itinerary!!.segments[2].earningResult!!.sqd)
+            assertEquals(242, itinerary!!.segments[3].earningResult!!.sqd)
+            assertEquals(1142, itinerary!!.segments[4].earningResult!!.sqd)
+            assertEquals(422, itinerary!!.segments[5].earningResult!!.sqd)
+        }
+
+        with(
+            SqdCalculator(
+                baseFare = 2792.0,
+                surcharges = 0.0,
+                ticket = "014",
+                aeroplanStatus = "100",
+                hasBonusPointsPrivilege = true,
+                segments = """
+                AC,YYC,YYZ,Y,LT
+                AC,YYZ,YUL,Y,LT
+                AC,YUL,BOG,Y,LT
+                AC,BOG,YUL,Y,LT
+                AC,YUL,YYC,Y,LT
+            """.trimIndent(),
+            ).calculate()
+        ) {
+            assertEquals(492, itinerary!!.segments[0].earningResult!!.sqd)
+            assertEquals(93, itinerary!!.segments[1].earningResult!!.sqd)
+            assertEquals(829, itinerary!!.segments[2].earningResult!!.sqd)
+        }
     }
 }
