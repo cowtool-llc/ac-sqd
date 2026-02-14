59d9be5ed298be60c67e5425ddcb275c44dce446
Scott Kennedy <scottkennedy@users.noreply.github.com>
2026-02-14 10:46:26 -0800

    Remove separate API handler (#525)
    This is no longer needed; the PNR tool calls the main handler.

--- scripts/update-sqd.sh
+++ scripts/update-sqd.sh
@@ -1,9 +1,7 @@
 #!/bin/bash -e
 
 MAIN_LAMBDA_NAME="ac-sqc"
-API_LAMBDA_NAME="ac-sqc-api"
 MAIN_HANDLER="com.cowtool.acsqd.SqdHandler::handleRequest"
-API_HANDLER="com.cowtool.acsqd.SqdApiHandler::handleRequest"
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 SCRIPT_DIR=$SCRIPT_DIR/../scripts
 ZIP_PATH="$SCRIPT_DIR/../build/libs/AC_SQD-all.jar"
@@ -15,9 +13,4 @@ aws lambda update-function-configuration \
   --function $MAIN_LAMBDA_NAME \
   --handler $MAIN_HANDLER
 
-aws lambda update-function-configuration \
-  --function $API_LAMBDA_NAME \
-  --handler $API_HANDLER
-
 "$SCRIPT_DIR"/update-lambda.sh $MAIN_LAMBDA_NAME "$ZIP_PATH"
-"$SCRIPT_DIR"/update-lambda.sh $API_LAMBDA_NAME "$ZIP_PATH"
